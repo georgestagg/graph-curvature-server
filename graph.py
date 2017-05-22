72ea385eda04d48d769dceab94bd4fc7ffaed4fe
George Stagg <george.stagg@newcastle.ac.uk>
2017-05-22 09:30:12 +0100

    Fix normalised finite d

--- graph.py
+++ graph.py
@@ -310,7 +310,7 @@ def dimpartNOR(A, i, n):
 	m = len(M)
 	R = [[0 for x in range(m)] for x in range(m)]
 	d = sum(A[i])
-	R[0][0] = d**2
+	R[0][0] = 1
 	for i in range(1, d+1):
 		R[0][i] = -1.0/(1.0*d)
 		R[i][0] = -1.0/(1.0*d)
