91e6db4dbbdb302d2ac7057696b434fac47c251f
George Stagg <georgestagg@gmail.com>
2021-07-26 11:20:09 +0100

    Add Ben's new algorithms

---
+++ curvature.py
@@ -0,0 +1,239 @@
+import numpy as np  # type: ignore[import]
+import copy
+from numpy.linalg import eigvalsh  # type: ignore[import]
+
+"""
+Graph curvature calculator functions, written by Ben Snodgrass.
+
+This code is based off formulae given in 'Bakry-Émery curvature on graphs as
+an eigenvalue problem' by Cushing et al. (2021).
+"""
+
+inf = float('inf')
+
+
+def non_normalised_unweighted_curvature(A, n):
+    """
+    Curvature calculator for an arbitrary simple, unweighted graph as a function
+    of the adjacency matrix and curvature dimension.
+
+    A is the adjacency matrix of the graph G each vertex has a arbitrarily
+    assigned vertex number, determined by position in A.
+    """
+
+    q = len(A)
+    curvlst = []
+
+    # Switch to Numpy to increase calculation speed
+    # In this case, A[i, j] = p_ij as mu[x] = 1 (mu is the measure) for all x, and there is no weighting
+    A = np.array(A, dtype=float)
+    
+    # list of one-spheres of the vertices
+    onesps = [[] for i in range(q)]
+    for i in range(q):
+        for j in range(q):
+            if A[i, j] == 1:
+                onesps[i].append(j)
+    # number of nearest neighbours of each vertex, in this case also the degree of each vertex
+    lenonesps = [len(onesps[i]) for i in range(q)]
+    # twosp is a matrix whose (i, j)th element = 0 iff i is in the two ball of j and vice versa
+    # if i and j have a common neighbour (and so could be in each other's two balls), A[i].A[j] must be positive
+    A_2 = np.matmul(A, A)
+    twosp = copy.copy(A_2)
+    for i in range(q):
+        for j in range(q):
+            # a point is not in its own two-ball
+            if i == j:
+                twosp[i, j] = 0
+            # also not in two ball if the vertices are adjacent
+            if A[i, j] == 1:
+                twosp[i, j] = 0
+            # more than one common neighbour is irrelevant to being in two-sphere or not
+            if twosp[i, j] != 0:
+                twosp[i, j] = 1
+            # create the following matrices to perform summations. Use np.matmul rather than list
+            # comprehension sums as they are far quicker this performs first summation in eq. A.11
+    sum2 = np.matmul(A, twosp)
+    # create matrix of reciprocals of p^(2)_ij with terms corresponding to vertices not in two_sphere(x)
+    # replaced by zero. This corresponds to only summing over z in S2(x)
+    recipA_2 = twosp*A_2
+    for i in range(q):
+        for j in range(q):
+            if recipA_2[i, j] != 0:
+                recipA_2[i, j] = 1/recipA_2[i, j]
+    # this performs the last summation in eq. A.11    
+    sum3 = np.matmul(recipA_2, A)
+    # (i, j)th element of first matrix is the list [p[i, z]*p[j, z] for z in range(q)], 
+    # which, dotted with recipA_2[x], gives the summation in A.12
+    sum4 = np.matmul([[[A[i, z]*A[j, z] for z in range(q)] for j in range(q)] for i in range(q)], recipA_2)
+    for x in range(q):
+        m = lenonesps[x]
+        # isolated points have default curvature 0
+        if m == 0:
+            curvlst.append(0)
+        else:
+            onesp = onesps[x]
+            # in the non-normalised, non-weighted case, (v_0)(v_0)^T is simplly a matrix of 1's
+            A_n = [[-2/n for i in range(m)] for j in range(m)]
+            for i in range(m):
+                # formula A.11 can be dramatically simplified by noting that p_xy = 1 for all y~x
+                # both p-terms in eq. A.13 are 1 and can be ignored
+                # d_x, the degree, is just m in this case
+                A_n[i][i] += 5/2-1/2*m+2*A_2[x, onesp[i]]+3/2*sum2[onesp[i], x]-2*sum3[x, onesp[i]]      
+            for i in range(m):
+                for j in range(m):
+                    if i != j:
+                        # again, p_xy = 1 for y~x simplifies eq. A.12
+                        A_n[i][j] += 1-2*A[onesp[i], onesp[j]]-2*sum4[onesp[i], onesp[j], x]
+            # eigvalsh returns list of eigenvalues of A_n, smallest first, so eigvalsh(A_n)[0] is the
+            # smallest eigenvalue of A_n, which is the curvature
+            curvlst.append(round(eigvalsh(A_n)[0], 3))
+    # returns list of curvature values at all vertices, ordered by vertex index    
+    return curvlst
+
+
+def normalised_unweighted_curvature(A, n):
+    """
+    Normalised Curvature calculator for an arbitrary simple, unweighted graph as a function
+    of the adjacency matrix and curvature dimension.
+
+    A is the adjacency matrix of the graph G. Each vertex has a arbitrarily
+    assigned vertex number, determined by position in A.
+    """
+    # Switch to Numpy to increase calculation speed
+    A = np.array(A, dtype=float)
+    q = len(A)
+    # list of one-spheres of the vertices
+    onesps = [[] for i in range(q)]
+    for i in range(q):
+        for j in range(q):
+            if A[i, j] == 1:
+                onesps[i].append(j)
+    # number of nearest neighbours of each vertex
+    lenonesps = [len(onesps[i]) for i in range(q)]
+    curvlst = []
+    # p_xy values for all combinations of vertices x, y in G
+    p = A*np.reciprocal(np.array([[lenonesps[i]] for i in range(q)], dtype=float))
+    # matrix p_2 = p^2 has the (x, z)th element identical to p^(2)_xz
+    p_2 = np.matmul(p, p)
+    # twosp is a matrix whose (i, j)th element = 0 iff i is in the two ball of j and vice versa
+    # if i and j have a common neighbour (and so could be in each other's two balls), p[i].p[j]
+    # must be positive, hence use of p_2
+    twosp = copy.copy(p_2)
+    for i in range(q):
+        for j in range(q):
+            # a point is not in its own two-ball
+            if i == j:
+                twosp[i, j] = 0
+            # also not in two ball if the vertices are adjacent
+            if A[i, j] == 1:
+                twosp[i, j] = 0
+            # more than one common neighbour is irrelevant to being in two-sphere or not
+            if twosp[i, j] != 0:
+                twosp[i, j] = 1
+    r = lenonesps[0]
+    # if graph is r-regular, calculation can be simplified
+    for i in range(1, q):
+        if lenonesps[i] == r:
+            # isolated points have 0 curvature
+            if r == 0:
+                return [0 for i in range(q)]
+                # In non-normalised case, the curvature is 1/r*(curvature in non-normalised case),
+                # so the calculation can be performed exactly the same as non-normalised case,
+                # with 1/r factor added at end
+                # Code is copied directly from non-normalised case, with annotations mostly unchanged.
+                # It treats it as non-normalised curvature, so p_xy = A_xy in this section
+            else:
+                # p_xy = 1/r*A_xy, so A^2 = A_2 = r**2*p_2
+                A_2 = r**2*p_2
+                # create the following matrices to perform summations. Use np.matmul rather than
+                # list comprehension sums as it is far quicker
+                # this performs second summation in eq. A.11
+                sum2 = np.matmul(A, twosp)
+                # create matrix of reciprocals of p^(2)_ij with terms corresponding to vertices not
+                # in two_sphere(x) removed. The zero entries do not figure in the calculation anyway
+                # so the values are irrelevant
+                recipA_2 = twosp*A_2
+                for i in range(q):
+                    for j in range(q):
+                        if recipA_2[i, j] != 0:
+                            recipA_2[i, j] = 1/recipA_2[i, j]
+                # this performs the last summation in eq. A.11    
+                sum3 = np.matmul(recipA_2, A)
+                # (i, j)th element of first matrix is the list [A[i, z]*A[j, z] for z in range(q)], which,
+                # dotted with recipA_2[x], gives the summation in A.12
+                sum4 = np.matmul([[[A[i, z]*A[j, z] for z in range(q)] for j in range(q)] for i in range(q)], recipA_2)
+                for x in range(q):
+                    onesp = onesps[x]
+                    # in the non-normalised, non-weighted case, (v_0)(v_0)^T is simplly a matrix of 1's
+                    A_n = [[-2/n for i in range(r)] for j in range(r)]
+                    for i in range(r):
+                        # formula A.11 can be dramatically simplified by noting that p_xy = 1 for all y~x
+                        # both p-terms in eq. A.13 are 1 and can be ignored
+                        # d_x, the degree, is r
+                        A_n[i][i] += 5/2-1/2*r+2*A_2[x, onesp[i]]+3/2*sum2[onesp[i], x]-2*sum3[x, onesp[i]]
+                    for i in range(r):
+                        for j in range(r):
+                            if i != j:
+                                # again, p_xy = 1 for y~x simplifies eq. A.12
+                                A_n[i][j] += 1-2*A[onesp[i], onesp[j]]-2*sum4[onesp[i], onesp[j], x]
+                    # eigvalsh returns list of eigenvalues of A_n, smallest first, so eigvalsh(A_n)[0]
+                    # is the smallest eigenvalue of A_n, which is the curvature
+                    # 1/r adds required normalisation
+                    curvlst.append(1/r*eigvalsh(A_n)[0])
+            # returns list of curvature values at all vertices, ordered by vertex index
+            return curvlst
+        else:
+            # create matrix of reciprocals of p^(2)_ij. If p^(2)_ij = 0, the will not figure in the calculation
+            # anyway so the value is irrelevant
+            recipp_2 = twosp*p_2
+            for i in range(q):
+                for j in range(q):
+                    if recipp_2[i, j] != 0:
+                        recipp_2[i, j] = 1/recipp_2[i, j]
+                        # create the following matrices to perform summations. Use np.matmul rather than list
+                        # comprehension sums as they are far quicker
+            # termwise squaring of p, psquare != p^2.
+            # Used to calculate final summation in A.11
+            psquare = np.square(p)
+            # this performs first summation in eq. A.11
+            sum1 = np.matmul(p, twosp)
+            # used in first part of second summation in A.11. Multiplication by A 'filters out' terms not
+            # corresponding to vertices in the one-sphere of a point
+            sum2 = np.matmul(p, A)
+            # the performs last summation in eq. A.11
+            sum3 = np.matmul(recipp_2, np.transpose(psquare))  
+            #   (i, j)th element of this is the list [p[i, z]*p[j, z] for z in range(q)], which is used in
+            # conjunction with recipp_2 in line ** to perform summation in eq. A.12 
+            sum4 = np.matmul([[[p[i, z]*p[j, z] for z in range(q)] for j in range(q)] for i in range(q)],
+                             np.transpose(recipp_2))
+            for x in range(q):
+                m = lenonesps[x]
+                # isolated points have default curvature 0
+                if m == 0:
+                    curvlst.append(0)
+                else:
+                    onesp = onesps[x]
+                    # create -2/n*(v_0)(v_0)^T component of A_n
+                    A_n = [[-2/n*(p[x, onesp[i]]*p[x, onesp[j]])**0.5 for i in range(m)] for j in range(m)]
+                    # add A_infinity terms, using the above 'sum' matrices
+                    for i in range(m):
+                        # since d_x = mu[x] (d_x is vertex degree, mu is the vertex weighting) in this case,
+                        # d_x/mu[x] in eq. A.11 is simply 1
+                        # on-diagonal terms use A.11 and A.13
+                        # divide through by p(x, onesp[i]) as in formula A.13
+                        A_n[i][i] += (p[x, onesp[i]]**2-1/2*p[x, onesp[i]]+3/2*p[x, onesp[i]]
+                                      * (p[onesp[i], x]+sum1[onesp[i], x])+3/2*p[x, onesp[i]]*sum2[onesp[i], x]
+                                      + 1/2*p_2[x, onesp[i]]-2*p[x, onesp[i]]**2*sum3[x, onesp[i]])/p[x, onesp[i]]
+                    for i in range(m):
+                        for j in range(m):
+                            if i != j:
+                                # off-diagonal terms use equations A.12 and A.13
+                                A_n[i][j] += (p[x, onesp[i]]*p[x, onesp[j]]-p[x, onesp[i]]*p[onesp[i], onesp[j]]
+                                              - p[x, onesp[j]]*p[onesp[j], onesp[i]]-2*p[x, onesp[i]]*p[x, onesp[j]]
+                                              * sum4[onesp[i], onesp[j], x])/(p[x, onesp[i]]*p[x, onesp[j]])**0.5
+                    # eigvalsh returns list of eigenvalues of A_n, smallest first, so eigvalsh(A_n)[0] is the
+                    # smallest eigenvalue of A_n, which is the curvature        
+                    curvlst.append(round(eigvalsh(A_n)[0], 3))
+            # returns list of curvature values at all vertices, ordered by vertex index
+            return curvlst
--- graph.py
+++ graph.py
@@ -4,179 +4,7 @@ import numpy as np  # type: ignore[import]
 import json
 from scipy.optimize import linprog  # type: ignore[import]
 import scipy  # type: ignore[import]
-
-
-def evs(A):
-    V = eigvalsh(A)
-    for i in range(len(V)):
-        V[i] = format(V[i], 'f')
-    return V
-
-
-def intersect(a, b):
-    return list(set(a) & set(b))
-
-
-def one_sphere(A, i):
-    n = len(A)
-    RES = []
-    for j in range(n):
-        if A[i][j] == 1:
-            RES.append(j)
-    return RES
-
-
-def two_sphere(A, i):
-    M = np.array(A)
-    M2 = np.dot(M, M)
-    n = len(A)
-    RES = []
-    V = M2[i]
-    for j in range(n):
-        if V[j] != 0:
-            if A[i][j] != 1:
-                if i != j:
-                    RES.append(j)
-    return RES
-
-
-def two_ball(A, i):
-    S1 = one_sphere(A, i)
-    S2 = two_sphere(A, i)
-    O = []
-    O.append(i)
-    O.extend(S1)
-    O.extend(S2)
-    n = len(O)
-    RES = [[0 for x in range(n)] for x in range(n)] 
-    for j in range(n):
-        for k in range(n):
-            RES[j][k] = A[O[j]][O[k]]
-    return RES
-
-
-def fourGammatwo(A, i):
-    B = two_ball(A, i)
-    n = len(B)
-    RES = [[0 for x in range(n)] for x in range(n)]
-    d = sum(B[0])
-    S1 = one_sphere(B, 0)
-    S2 = two_sphere(B, 0)
-    E = [len(intersect(one_sphere(B, j), S1)) for j in range(n)]
-    F = [len(intersect(one_sphere(B, j), S2)) for j in range(n)]
-    RES[0][0] = d*(d+3)
-    for j in S1:
-        RES[j][j] = 5-d+3*F[j]+4*E[j]
-        RES[0][j] = -3-d-F[j]  
-        RES[j][0] = -3-d-F[j]
-    for j in S2:
-        RES[j][j] = E[j]
-        RES[0][j] = E[j] 
-        RES[j][0] = E[j]
-    for j in S1:
-        for k in S1:
-            if j != k:
-                RES[j][k] = 2 - 4*B[j][k]
-    for j in S1:
-        for k in S2:
-            RES[j][k] = -2*B[j][k]
-            RES[k][j] = -2*B[j][k]  
-    return RES
-
-
-def mu_two_ball(A, MU, i):
-    S1 = one_sphere(A, i)
-    S2 = two_sphere(A, i)
-    O = []
-    O.append(i)
-    O.extend(S1)
-    O.extend(S2)
-    n = len(O)
-    RES = [0 for x in range(n)]
-    for j in range(n):
-        RES[j] = MU[O[j]]
-    return RES
-
-
-def fourGammatwoMu(A, MU, i):
-    B = two_ball(A, i)
-    M = mu_two_ball(A, MU, i)
-    n = len(B)
-    RES = [[0 for x in range(n)] for x in range(n)]
-    d = sum(B[0])
-    mx = 1.0*M[0]
-    S1 = one_sphere(B, 0)
-    S2 = two_sphere(B, 0)
-    RES[0][0] = (1.0*d*d)/(mx**2)+(3.0/(mx))*sum([1.0/(1.0*M[y]) for y in S1])
-    for j in S1:
-        RES[j][j] = 3.0/(mx*M[j])+2.0/(mx**2)-(1.0*d)/(mx**2)+(3.0/(mx*M[j]))*sum([B[j][z] for z in S2])+(1.0/mx)*sum([(1.0/(1.0*M[z])+3.0/(1.0*M[j]))*B[j][z] for z in S1])
-        RES[0][j] = -3.0/(mx*M[j])-(1.0*d)/(mx**2)-(1.0/(mx*M[j]))*sum([B[j][z] for z in S2])-(1.0/mx)*sum([(-1.0/(1.0*M[z])+1.0/(1.0*M[j]))*B[j][z] for z in S1])
-        RES[j][0] = -3.0/(mx*M[j])-(1.0*d)/(mx**2)-(1.0/(mx*M[j]))*sum([B[j][z] for z in S2])-(1.0/mx)*sum([(-1.0/(1.0*M[z])+1.0/(1.0*M[j]))*B[j][z] for z in S1])
-    for j in S2:
-        RES[j][j] = (1.0/mx)*sum((1.0/M[y])*B[y][j] for y in S1)
-        RES[0][j] = (1.0/mx)*sum((1.0/M[y])*B[y][j] for y in S1)
-        RES[j][0] = (1.0/mx)*sum((1.0/M[y])*B[y][j] for y in S1)
-    for j in S1:
-        for k in S1:
-            if j != k:
-                RES[j][k] = 2.0/(mx**2)-(1.0/mx)*(2.0/(1.0*M[j]) + 2.0/(1.0*M[k]))*B[j][k] 
-    for j in S1:
-        for k in S2:
-            RES[j][k] = (-2.0*B[j][k])/(mx*M[j])
-            RES[k][j] = (-2.0*B[j][k])/(mx*M[j])
-    return RES
-
-
-def fourGammatwoNorm(A, i):
-    MU = [sum([A[j][k] for k in range(len(A))]) for j in range(len(A))]
-    return fourGammatwoMu(A, MU, i)
-
-
-def curvature_sign(A, i):
-    M = fourGammatwo(A,i)
-    ev = evs(M)
-    if ev[0]<0:
-        return -1
-    if ev[1] == 0:
-        return 0
-    else:
-        return 1
-
-
-def curvature_sign_norm(A, i):
-    M = fourGammatwoNorm(A, i)
-    ev = evs(M)
-    if ev[0] < 0:
-        return -1
-    if ev[1] == 0:
-        return 0
-    else:
-        return 1
-
-
-def fourGamma(A, i):
-    B = two_ball(A, i)
-    n = len(B)
-    RES = [[0 for x in range(n)] for x in range(n)]
-    RES[0][0] = 2*sum(B[0])
-    for j in range(1,n):
-        if B[0][j] == 1:
-            RES[j][j] = 2
-            RES[0][j] = -2
-            RES[j][0] = -2
-    return RES
-
-
-def fourGammaNorm(A, i):
-    d = sum(A[i])
-    M = (1.0/(1.0*d))*np.array(fourGamma(A, i))
-    return M.tolist()
-
-
-def brange(a, b, n):
-    d = (b-a)/float(n)
-    res = [a+i*d for i in range(n+1)]
-    return res
+from curvature import inf, normalised_unweighted_curvature, non_normalised_unweighted_curvature  # type: ignore[import]
 
 
 def Amat(n, m):
@@ -206,7 +34,7 @@ def etap(n, m, p):
     res[n] = -p
     for i in range(1, n):
         res[i] = (p-1.0)/(n-1)
-    for i in range(n+1,n+m):
+    for i in range(n+1, n+m):
         res[i] = (p-1.0)/(m-1)
     return res
 
@@ -223,8 +51,6 @@ def dist(i, j, A):
 
 
 def d(x, y, A):
-    n = sum(A[x])+1
-    m = sum(A[y])+1
     res = []
     xnbs = [x]
     ynbs = [y]
@@ -239,179 +65,11 @@ def d(x, y, A):
     return res
 
 
-def curv_calc(A, i):
-    M = fourGammatwo(A, i)
-    N = fourGamma(A, i)
-    ev = evs(M)
-    if ev[1] == 0 and ev[0] == 0:
-        return 0
-    if ev[0] < 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K -= sig
-                if evs(np.array(M)-K*np.array(N))[0] == 0:
-                    t += 1
-            K += sig
-            t = 0
-            sig /= 10.0
-        return round(K, 3)
-    if ev[1] > 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K += sig
-                if evs(np.array(M)-K*np.array(N))[0] < 0:
-                    t += 1
-            K -= sig
-            t = 0
-            sig /= 10.0
-        return round(K-(sig*10.0), 3)
-
-
-def curv_calc_norm(A, i):
-    M = fourGammatwoNorm(A, i)
-    N = fourGammaNorm(A, i)
-    ev = evs(M)
-    if ev[1] == 0 and ev[0] == 0:
-        return 0
-    if ev[0] < 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K -= sig
-                if evs(np.array(M)-K*np.array(N))[0] == 0:
-                    t += 1
-            K += sig
-            t = 0
-            sig /= 10.0
-        return round(K, 3)
-    if ev[1] > 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K += sig
-                if evs(np.array(M)-K*np.array(N))[0] < 0:
-                    t += 1
-            K -= sig
-            t = 0
-            sig /= 10.0
-        return round(K-(sig*10.0), 3)
-
-
-def dimpart(A, i, n):
-    M = two_ball(A, i)
-    m = len(M)
-    R = [[0 for x in range(m)] for x in range(m)]
-    d = sum(A[i])
-    R[0][0] = d**2
-    for i in range(1, d+1):
-        R[0][i] = -d
-        R[i][0] = -d
-    for i in range(1, d+1):
-        for j in range(1,d+1):
-            R[i][j] = 1
-    RES = (4.0/(1.0*n))*np.array(R)
-    return RES
-
-
-def dimpartNOR(A, i, n):
-    M = two_ball(A, i)
-    m = len(M)
-    R = [[0 for x in range(m)] for x in range(m)]
-    d = sum(A[i])
-    R[0][0] = 1
-    for i in range(1, d+1):
-        R[0][i] = -1.0/(1.0*d)
-        R[i][0] = -1.0/(1.0*d)
-    for i in range(1, d+1):
-        for j in range(1, d+1):
-            R[i][j] = 1.0/(1.0*d*d)
-    RES = (4.0/(1.0*n))*np.array(R)
-    return RES
-
-
-def dim_curv_calc(A, i, n):
-    M = np.array(fourGammatwo(A, i))-dimpart(A, i, n)
-    N = fourGamma(A, i)
-    ev = evs(M)
-    if ev[1] == 0 and ev[0] == 0:
-        return 0
-    if ev[0] < 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K -= sig
-                if evs(np.array(M)-K*np.array(N))[0] >= 0:
-                    t += 1
-            K += sig
-            t = 0
-            sig /= 10.0
-        return round(K, 3)
-    if ev[1] > 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K += sig
-                if evs(np.array(M)-K*np.array(N))[0] < 0:
-                    t += 1
-            K -= sig
-            t = 0
-            sig /= 10.0
-        return round(K-(sig*10.0), 3)
-
-
-def dim_curv_calc_norm(A, i, n):
-    M = np.array(fourGammatwoNorm(A, i))-dimpartNOR(A, i, n)
-    N = fourGammaNorm(A, i)
-    ev = evs(M)
-    if ev[1] == 0 and ev[0] == 0:
-        return 0
-    if ev[0] < 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K -= sig
-                if evs(np.array(M)-K*np.array(N))[0] >= 0:
-                    t += 1
-            K += sig
-            t = 0
-            sig /= 10.0
-        return round(K, 3)
-    if ev[1] > 0:
-        K = 0
-        t = 0
-        sig = 1
-        while sig > 0.00001:
-            while t == 0:
-                K += sig
-                if evs(np.array(M)-K*np.array(N))[0] < 0:
-                    t += 1
-            K -= sig
-            t = 0
-            sig /= 10.0
-        return round(K-(sig*10.0), 3)
-
-
 def ocurve(x, y, A):
     dx = sum(A[x])
     dy = sum(A[y])
     return 1+scipy.optimize.OptimizeResult.values(
-        linprog(c=eta(dx+1, dy+1),A_ub=Amat(dx+1, dy+1), b_ub=d(x, y, A), bounds=(None, None)))[3]
+        linprog(c=eta(dx+1, dy+1), A_ub=Amat(dx+1, dy+1), b_ub=d(x, y, A), bounds=(None, None)))[3]
 
 
 def lazocurve(x, y, A, p):
@@ -446,6 +104,10 @@ urls = (
 web.config.debug = False
 
 
+def sign(x):
+    return -1 if x < 0 else (1 if x > 0 else 0)
+
+
 class index:
     def POST(self):
         web.header('Access-Control-Allow-Origin', '*')
@@ -459,35 +121,29 @@ class index:
 
         if t == 0:
             try:
-                ret = range(len(V));
+                ret = range(len(V))
             except Exception:
                 return '["error3"]'
         if t == 1:
             try:
-                ret = []
-                for j in range(len(V)):
-                    ret.append(curvature_sign(AM, j))
+                ret = non_normalised_unweighted_curvature(AM, inf)
+                ret = [sign(j) for j in ret]
             except Exception:
                 return '["error1"]'
         if t == 2:
             try:
-                ret = []
-                for j in range(len(V)):
-                    ret.append(curvature_sign_norm(AM, j))
+                ret = normalised_unweighted_curvature(AM, inf)
+                ret = [sign(j) for j in ret]
             except Exception:
                 return '["error4"]'
         if t == 3:
             try:
-                ret = []
-                for j in range(len(V)):
-                    ret.append(curv_calc(AM, j))
+                ret = non_normalised_unweighted_curvature(AM, inf)
             except Exception:
                 return '["error5"]'
         if t == 4:
             try:
-                ret = []
-                for j in range(len(V)):
-                    ret.append(curv_calc_norm(AM, j))
+                ret = normalised_unweighted_curvature(AM, inf)
             except Exception:
                 return '["error6"]'
         if t == 5:
@@ -500,9 +156,7 @@ class index:
             except Exception:
                 return '["error9"]'
             try:
-                ret = []
-                for j in range(len(V)):
-                    ret.append(dim_curv_calc(AM, j, dimn))
+                ret = ret = non_normalised_unweighted_curvature(AM, dimn)
             except Exception:
                 return '["error7"]'
         if t == 6:
@@ -527,9 +181,7 @@ class index:
             except Exception:
                 return '["error9"]'
             try:
-                ret = []
-                for j in range(len(V)):
-                    ret.append(dim_curv_calc_norm(AM, j, dimn))
+                ret = normalised_unweighted_curvature(AM, dimn)
             except Exception:
                 return '["error11"]'
 
